f736eb72bc3b27b86efbe90c9d62af397a27f929
asdf <dregdog1234@gmail.com>
2019-10-07 14:27:36 +0900

    callender

--- li0930/calll.sh
+++ li0930/calll.sh
@@ -1,17 +1,28 @@
 #2019년 9월 달력 출력
 echo -e "\t10월 2019"
 echo -e "일 월 화 수 목 금 토"
-p=1
+p=3
 dayy=1
-while [ $dayy -le 30 ]
-		do
-		if [ $p -gt 7 ]
-		then
-			p=`expr $p - 7`
-			echo -ne "\n"
-		fi
-		echo -n "$dayy   "
-		dayy=`expr $dayy + 1`
-		p=`expr $p + 1`
-done	
-				
+cnt=1
+while [ $cnt -lt $p ]
+do
+	echo -n "   "
+	cnt=`expr $cnt + 1`
+done
+while [ $dayy -le 31 ]
+do
+	if [ $p -gt 7 ]
+	then
+		p=`expr $p - 7`
+		echo -ne "\n"
+	fi
+	if [ $dayy -gt 9 ]
+	then
+		echo -n "$dayy "
+	else
+		echo -n " $dayy "
+	fi
+	dayy=`expr $dayy + 1`
+	p=`expr $p + 1`
+done
+echo		
